ced955adba99e63d43b6cdf156b860ece526cac2
3bdo <114808114+3bdoMorsyy@users.noreply.github.com>
2024-10-18 10:44:32 +0300

    Add files via upload

---
+++ js/sc.js
@@ -0,0 +1,19 @@
+document.getElementById("change-theme").onclick = function () {
+  document.body.classList.toggle("dark-color");
+  document.querySelector(".navbar").classList.toggle("black-navbar");
+  document.querySelector(".fa-moon").classList.toggle("d-none");
+  document.querySelector(".fa-sun-bright").classList.toggle("d-none");
+};
+document.querySelector(".fa-bars").onclick = function () {
+  document.querySelector(".navbar .container ul").classList.toggle("clicked");
+};
+
+allLis = document.querySelectorAll(".navbar .links li a");
+allLis.forEach(function (e) {
+  e.onclick = function () {
+    allLis.forEach(function (ele) {
+      ele.classList.remove("active");
+    });
+    e.classList.add("active");
+  };
+});
